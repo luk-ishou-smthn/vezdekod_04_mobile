c782e72e845ffbc3c831ac007b02518d08b125e8
vldplcd <shilov.part@gmail.com>
2020-09-12 11:05:36 +0300

    Adding Project

---
+++ app/src/main/java/com/example/vezdekod/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.vezdekod
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        button_send.setOnClickListener{
+            val intent = Intent(this, SecondStep::class.java)
+            startActivity(intent)
+        }
+
+    }
+}
---
+++ app/src/main/java/com/example/vezdekod/SecondStep.kt
@@ -0,0 +1,17 @@
+package com.example.vezdekod
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class SecondStep : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.second_step)
+
+    }
+
+}
